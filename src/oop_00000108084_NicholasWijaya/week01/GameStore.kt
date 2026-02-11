d95f0dbd5ccc1f617c5c6e623a61e7416802f437
unknown <nicholas02233@gmail.com>
2026-02-11 15:47:25 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000108084_NicholasWijaya/week01/GameStore.kt
+++ src/oop_00000108084_NicholasWijaya/week01/GameStore.kt
@@ -5,4 +5,9 @@ fun main() {
     val gameTitle = "Persona 3 Reload"
     val price = 579000
 
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 80) / 100
+    else (price * 90) / 100
