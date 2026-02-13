0f27f0d641be37664dfe32f44ce4e353fdcd28f4
unknown <nicholas02233@gmail.com>
2026-02-13 10:39:47 +0700

    week02: create basic Student class structure

--- src/ oop_00000108084_NicholasWijaya/week02/Student.kt
+++ src/ oop_00000108084_NicholasWijaya/week02/Student.kt
@@ -3,7 +3,7 @@ package ` oop_00000108084_NicholasWijaya`.week02
 class Student(
     val name: String,
     val nim: String,
-    val major: String,
+    var major: String
 ) {
     // Body class kosong dulu
 }
