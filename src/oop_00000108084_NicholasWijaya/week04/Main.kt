a8ca8af453974b481518116bacd96e96d33cd6a5
unknown <nicholas02233@gmail.com>
2026-02-27 11:22:42 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000108084_NicholasWijaya/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000108084_NicholasWijaya.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Lamborghini", numberOfDoors = 2)
+    myCar.openTrunk() // Memanggil method milik sendiri
+    myCar.honk()      // Memanggil method yang sudah di-override
+    myCar.accelerate()// Memanggil gabungan method Parent dan Child
+}
