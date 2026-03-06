16a621d667999d0c03fd5adbf570a300613dc96c
unknown <nicholas02233@gmail.com>
2026-03-06 10:53:54 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000108084_NicholasWijaya/Week05/mainMath.kt
@@ -0,0 +1,12 @@
+package oop_00000108084_NicholasWijaya.Week05
+
+fun main() {
+
+    val math = MathHelper()
+
+    println("--- HITUNG LUAS ---")
+    println("Luas Persegi: " + math.hitungLuas(4))
+    println("Luas Persegi Panjang: " + math.hitungLuas(5, 3))
+    println("Luas Lingkaran: " + math.hitungLuas(7.0))
+
+}
