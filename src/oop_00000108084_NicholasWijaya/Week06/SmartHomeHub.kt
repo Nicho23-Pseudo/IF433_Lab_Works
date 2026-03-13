d2d8515d61fd511ef1af954899658ffa9ade23e2
unknown <nicholas02233@gmail.com>
2026-03-13 10:59:25 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000108084_NicholasWijaya/Week06/SmartHomeHub.kt
+++ src/oop_00000108084_NicholasWijaya/Week06/SmartHomeHub.kt
@@ -1,5 +1,7 @@
 package oop_00000108084_NicholasWijaya.Week06
 
+import java.security.Security
+
 class SmartHomeHub {
 
     val devices = mutableListOf<SmartDevice>()
@@ -12,6 +14,19 @@ class SmartHomeHub {
         for (device in devices) {
             if (device is Switchable) {
                 device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
             }
         }
     }
