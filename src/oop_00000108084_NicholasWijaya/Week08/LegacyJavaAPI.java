1f1e584f30950c7957761e4a5f2fed13849cc841
unknown <nicholas02233@gmail.com>
2026-04-17 11:23:41 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000108084_NicholasWijaya/Week08/LegacyJavaAPI.java
+++ src/oop_00000108084_NicholasWijaya/Week08/LegacyJavaAPI.java
@@ -3,6 +3,6 @@ package oop_00000108084_NicholasWijaya.Week08;
 public class LegacyJavaAPI {
     // Fungsi ini ditulis dalam Java. kotlin tidak tahu apakah kembaliannya Nullable.
     public static String fetchServerStatus() {
-        return "SERVER_ONLINE200_OK";
+        return "SERVER_ONLINE_200_OK";
     }
 }
