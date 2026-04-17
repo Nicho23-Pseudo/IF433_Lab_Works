bd011623cfd080d7b721385d7b292f17c86215b0
unknown <nicholas02233@gmail.com>
2026-04-17 11:00:32 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000108084_NicholasWijaya/Week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000108084_NicholasWijaya.Week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE200_OK";
+    }
+}
