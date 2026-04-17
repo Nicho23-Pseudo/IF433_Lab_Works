3b93d2325fb71401b830ad3df8012daf8bf97c6a
unknown <nicholas02233@gmail.com>
2026-04-17 10:42:30 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000108084_NicholasWijaya/Week08/Main.kt
+++ src/oop_00000108084_NicholasWijaya/Week08/Main.kt
@@ -23,8 +23,17 @@ fun main() {
     val mixedData: List<Any> = listOf(
         "Smartphone",
         1500000,
-        UserProfile("Andi", null)
+        UserProfile("Andi", null),
         "Laptop",
         4500000.0
     )
+
+    for (item in mixedData) {
+        val text = item as? String
+
+        // Hanya cetak jika cast sukses (text tidak null)
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
