84529246ca5b06e9bd79d48b2347c6014b134589
unknown <nicholas02233@gmail.com>
2026-04-24 11:11:01 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000108084_NicholasWijaya/Week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000108084_NicholasWijaya.Week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 20.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 8.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 5, -2.5, "OPEN")
+    )
+}
