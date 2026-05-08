fe21d2b116a900536d71867adba9dcf14c1dffec
unknown <nicholas02233@gmail.com>
2026-05-08 10:09:47 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000108084_NicholasWijaya/Week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000108084_NicholasWijaya.Week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
