062506f84258179f7a067758e52eb1a1df025bad
unknown <nicholas02233@gmail.com>
2026-05-29 10:52:52 +0700

    week14: (task) copy bad E-Commerce OrderProcessor code

--- src/oop_00000108084_NicholasWijaya/Week14/EcommerceTask.kt
+++ src/oop_00000108084_NicholasWijaya/Week14/EcommerceTask.kt
@@ -1,26 +1,61 @@
 package oop_00000108084_NicholasWijaya.Week14
 import java.io.File
 
-class BadOrderProcessor {
+interface OrderRepository {
+    fun saveOrder(customerType: String, finalPrice: Double, email: String)
+}
 
-    fun processOrder(customerType: String, amount: Double, email: String) {
+class CsvOrderRepository : OrderRepository {
 
-        // OCP Violation
-        val finalPrice = when (customerType) {
-            "VIP" -> amount * 0.8
-            "Regular" -> amount
-            else -> amount
+    override fun saveOrder(
+        customerType: String,
+        finalPrice: Double,
+        email: String
+    ) {
+
+        File("orders.csv").bufferedWriter().use { writer ->
+            writer.append("$customerType,$finalPrice,$email\n")
         }
+    }
+}
 
-        // DIP + SRP Violation
-        File("orders.csv").appendText("$customerType,$finalPrice,$email\n")
+interface NotificationService {
+    fun sendNotification(email: String, finalPrice: Double)
+}
 
-        // SRP Violation
+class EmailNotifier : NotificationService {
+
+    override fun sendNotification(email: String, finalPrice: Double) {
         println("Sending email to $email for total payment: $finalPrice")
     }
 }
 
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+
+    fun processOrder(
+        customerType: String,
+        finalPrice: Double,
+        email: String
+    ) {
+
+        repo.saveOrder(customerType, finalPrice, email)
+        notifier.sendNotification(email, finalPrice)
+    }
+}
+
 fun main() {
-    val processor = BadOrderProcessor()
-    processor.processOrder("VIP", 500000.0, "customer@gmail.com")
+
+    val repository = CsvOrderRepository()
+    val notifier = EmailNotifier()
+
+    val processor = SafeOrderProcessor(repository, notifier)
+
+    processor.processOrder(
+        "VIP",
+        400000.0,
+        "customer@gmail.com"
+    )
 }
